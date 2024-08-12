69fd1d1c6f18d0947cd1ba518447147466edeb4c
agsant <agussuryanto.work@gmail.com>
2024-08-12 08:28:42 +0700

    fixing text input handler

--- lib/src/widgets/ags_checkbox.dart
+++ lib/src/widgets/ags_checkbox.dart
@@ -4,24 +4,29 @@ class AgsCheckbox extends StatelessWidget {
   final bool value;
   final bool readOnly;
   final void Function(bool)? onChanged;
+  final VoidCallback? onTap;
 
   const AgsCheckbox({
     super.key,
     this.value = false,
     this.readOnly = false,
     this.onChanged,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Checkbox(
-      value: value,
-      onChanged: (result) {
-        if (readOnly) {
-          return;
-        }
-        onChanged?.call(result ?? false);
-      },
+    return GestureDetector(
+      onTap: onTap,
+      child: Checkbox(
+        value: value,
+        onChanged: (result) {
+          if (readOnly) {
+            return;
+          }
+          onChanged?.call(result ?? false);
+        },
+      ),
     );
   }
 }
--- lib/src/widgets/ags_textfield_tiles/ags_textfield_item.dart
+++ lib/src/widgets/ags_textfield_tiles/ags_textfield_item.dart
@@ -45,10 +45,17 @@ class _AgsTextfieldItemState extends State<AgsTextfieldItem> {
                 event.logicalKey.keyLabel == keyBackspaceLabel)) {
           widget.onRemove?.call();
         }
-        return KeyEventResult.handled;
+        return KeyEventResult.ignored;
       },
     );
     _focusNode.requestFocus();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _focusNode.dispose();
+    super.dispose();
   }
 
   @override
