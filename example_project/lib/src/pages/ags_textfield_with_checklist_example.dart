20c08bc1b5fed629c71356e8ce1ef31acbae1c77
agsant <agussuryanto.work@gmail.com>
2024-09-08 21:27:28 +0700

    fixing callback onUpdated

--- example_project/lib/src/pages/ags_textfield_with_checklist_example.dart
+++ example_project/lib/src/pages/ags_textfield_with_checklist_example.dart
@@ -29,7 +29,7 @@ class _AgsTextfieldWithChecklistExampleState
                 enableInputBorder: true,
                 controller: _controller,
                 checklistType: false,
-                onDataUpdated: (items) {},
+                onDataUpdated: (items, index) {},
               ),
             ),
             Container(
