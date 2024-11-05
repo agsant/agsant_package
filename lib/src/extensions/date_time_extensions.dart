422ee2297fd17f3bfa709c616818bb67a617ecef
agsant <agussuryanto.work@gmail.com>
2024-11-05 11:55:57 +0700

    fixing date format

--- lib/src/extensions/date_time_extensions.dart
+++ lib/src/extensions/date_time_extensions.dart
@@ -1,9 +1,16 @@
 import 'package:intl/intl.dart';
 
 extension DateTimeExtension on DateTime {
+  @Deprecated('Use format(...) instead, to support locale')
   String stringFormat({String? format}) {
     String string = format ?? 'dd MMMM yyyy';
     DateFormat dateFormat = DateFormat(string);
+    return dateFormat.format(this);
+  }
+
+  String format({String? format, String? locale}) {
+    String string = format ?? 'dd MMMM yyyy';
+    DateFormat dateFormat = DateFormat(string, locale);
     return dateFormat.format(this);
   }
 }
